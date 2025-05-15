499caec93a6e8bd3ca1f5b34a124c62f74a32746
desraymondz <desraymondz@gmail.com>
2025-05-15 23:55:59 +0800

    fixed google form height

--- src/components/home/GoogleForm.jsx
+++ src/components/home/GoogleForm.jsx
@@ -3,7 +3,7 @@ export default function FeaturesSection() {
         <iframe 
             src="https://docs.google.com/forms/d/e/1FAIpQLScptiA9s0nt-ik-PuC7tVw0tdAjiUmsTNlVA9D-b0Rz2roHDw/viewform?embedded=true" 
             width="100%" 
-            height="3205" 
+            height="900" 
             frameborder="0" 
             marginheight="0" 
             marginwidth="0">
